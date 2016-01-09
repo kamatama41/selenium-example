888d87146c6065a41ab11163b779161a4c708c89
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-01-09 22:27:34 +0900

    Add description and screenshot directory

--- src/main/java/com/kamatama41/ChromeTest.java
+++ src/main/java/com/kamatama41/ChromeTest.java
@@ -26,29 +26,40 @@ public class ChromeTest {
                 .build();
 
         for(final String q : Arrays.asList("ゲスの極み", "川谷絵音", "休日課長", "ほないこか", "ちゃんMARI")) {
+            // Driverを初期化
             WebDriver driver = new ChromeDriver(service);
 
+            // Googleを開く
             driver.get("https://google.com");
 
+            // "q"というnameのelementを探す(検索ボックスを探す)
             WebElement element = driver.findElement(By.name("q"));
 
+            // 検索ボックスに文字を入力する
             element.sendKeys(q);
 
+            // Submitボタンを押す
             element.submit();
 
+            // 現在のページタイトルをチェック
             System.out.println("タイトル: " + driver.getTitle());
 
+            // 読み込みが終わるまで待つ
+            // タイトルが想定したものかどうかをチェック
             (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
                 public Boolean apply(WebDriver d) {
                     return d.getTitle().startsWith(q);
                 }
             });
 
+            // 現在のページタイトルをチェック
             System.out.println("タイトル: " + driver.getTitle());
 
+            // スクリーンショットを取る
             WebDriver augmentedDriver = new Augmenter().augment(driver);
             File screenshot = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
 
+            // 取ったファイルをscreenshot/{クエリ}_yyyyMMddHHmmss.jpg として保存
             String DATE_FORMAT = "yyyyMMddHHmmss";
             Path dst = Paths.get(".", "screenshot", q + "_" + new SimpleDateFormat(DATE_FORMAT).format(new Date()) + ".jpg");
             try {
@@ -57,6 +68,7 @@ public class ChromeTest {
                 e.printStackTrace();
             }
 
+            // Driverを閉じる
             driver.quit();
         }
     }
